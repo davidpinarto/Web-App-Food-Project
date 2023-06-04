696d9114d98ce53f1907d2dd263113ff125f8ed7
David Pinarto <davidpinarto90@gmail.com>
2023-06-04 20:34:06 +0800

    add code to main and footer element html with style

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,8 +1,8 @@
 const testAPI = async () => {
   try {
-    const response = await fetch('https://themealdb.com/api/json/v1/1/search.php?f=c');
+    const response = await fetch('https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
     const responseJson = await response.json();
-    console.log(responseJson.meals.length);
+    console.log(responseJson.meals[0].strInstructions);
   } catch (error) {
     console.log(error);
   }

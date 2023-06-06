349370f3a78961bcfbbaa227e1c39320312af912
David Pinarto <davidpinarto90@gmail.com>
2023-06-06 20:30:26 +0800

    fix modal card-item

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,13 +1,27 @@
-const testAPI = async () => {
-  try {
-    const response = await fetch('https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
-    const responseJson = await response.json();
-    console.log(responseJson.meals[0].strInstructions);
-  } catch (error) {
-    console.log(error);
+const main = () => {
+  const testAPI = async () => {
+    try {
+      const response = await fetch('https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
+      const responseJson = await response.json();
+      console.log(responseJson.meals[0].strInstructions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // testAPI();
+
+  // toggle pop up
+  const popUp = document.getElementById('card-pop-up');
+
+  function togglePopUp() {
+    popUp.classList.toggle('active');
+    if (popUp.matches('.active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 };
 
-// testAPI();
-
-
+export default main;

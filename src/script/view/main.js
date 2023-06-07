916239a38ac06d4cdccbb78f8067ef7fae1d29e4
David Pinarto <davidpinarto90@gmail.com>
2023-06-07 21:55:19 +0800

    add navbar responsive feature

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -13,15 +13,48 @@ const main = () => {
 
   // toggle pop up
   const popUp = document.getElementById('card-pop-up');
+  const readMoreBtn = document.querySelectorAll('.pop-up-trigger-btn');
 
-  function togglePopUp() {
-    popUp.classList.toggle('active');
-    if (popUp.matches('.active')) {
+  readMoreBtn.forEach((value) => {
+    console.log(value);
+    value.addEventListener('click', () => {
+      popUp.classList.toggle('active');
+      if (popUp.matches('.active')) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
+    });
+  });
+
+  // nav-bars
+  const topNavigation = document.querySelector('.top-navigation');
+  const navBars = document.querySelector('.nav-bars');
+  const navXMark = document.querySelector('.nav-xmark');
+  const navOverlay = document.querySelector('.nav-overlay');
+  console.log(navXMark);
+
+  navBars.addEventListener('click', () => {
+    topNavigation.classList.toggle('nav-bars-active');
+    navXMark.classList.toggle('nav-xmark-active');
+    navOverlay.classList.toggle('.nav-overlay-active');
+    if (topNavigation.matches('.top-navigation.nav-bars-active')) {
       document.body.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'auto';
     }
-  }
+  });
+
+  navXMark.addEventListener('click', () => {
+    topNavigation.classList.toggle('nav-bars-active');
+    navXMark.classList.toggle('nav-xmark-active');
+    navOverlay.classList.toggle('.nav-overlay-active');
+    if (topNavigation.matches('.top-navigation.nav-bars-active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  });
 };
 
 export default main;
